e10f0d1d11eee13e65af6c32772643de1f57537d
1131153523 <1131153523@qq.com>
2019-01-20 22:26:13 +0800

    上传最新数据库信息

--- website.sql
+++ website.sql
@@ -1,7 +1,7 @@
 /*
 Navicat MySQL Data Transfer
 
-Source Server         : fanyihui
+Source Server         : fyh
 Source Server Version : 50721
 Source Host           : localhost:3306
 Source Database       : website
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50721
 File Encoding         : 65001
 
-Date: 2019-01-17 14:01:59
+Date: 2019-01-20 22:22:17
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -31,7 +31,7 @@ CREATE TABLE `admin` (
 -- ----------------------------
 -- Records of admin
 -- ----------------------------
-INSERT INTO `admin` VALUES ('1', 'admin', '123', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiYWRtaW4iLCJpYXQiOjE1NDc3MDE2OTUsImV4cCI6MTU0NzcxMjQ5NX0.dzRwuT8skQ2M44He4myMNIDKW_GDCjSgAxbJpMgRT7o', '2019-1-17 下午1:08:15');
+INSERT INTO `admin` VALUES ('1', 'admin', '123', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiYWRtaW4iLCJpYXQiOjE1NDc5ODgzOTcsImV4cCI6MTU0Nzk5OTE5N30.VtDn5gaIYXhANE_ifKgyzkmauWqKNNPZWce9U8WoyK0', '2019-1-20 下午8:46:37');
 
 -- ----------------------------
 -- Table structure for articles
@@ -58,11 +58,11 @@ CREATE TABLE `articles` (
 -- Records of articles
 -- ----------------------------
 INSERT INTO `articles` VALUES ('0652309127', '清除浮动的六种方式', '', '1', 'admin', '/home/fanyihui/Desktop/web-demo/server/static/articles/清除浮动的六种方式.md', '23', '0', '', '2019-1-16 下午6:19:54', 'f11d873b99');
-INSERT INTO `articles` VALUES ('06b49d8513', 'js实现插入排序', '3333', '1', 'admin', '/home/fanyihui/Desktop/web-demo/server/static/articles/js实现插入排序.md', '0', '0', '', '2019-1-16 下午6:19:54', '3a868d8e6b');
-INSERT INTO `articles` VALUES ('6ad6bf1cb4', '动手实现一个简单的Promise', '44444', '1', 'admin', '/home/fanyihui/Desktop/web-demo/server/static/articles/动手实现一个简单的Promise.md', '0', '0', '', '2019-1-16 下午6:19:54', '15f50360a3');
-INSERT INTO `articles` VALUES ('95e00e13fa', 'Vue生命周期函数', '', '1', 'admin', '/home/fanyihui/Desktop/web-demo/server/static/articles/Vue生命周期函数.md', '5', '0', '', '2019-1-16 下午6:19:54', '6df93151c8');
-INSERT INTO `articles` VALUES ('d04eac5996', 'JS类型判断', '22222', '1', 'admin', '/home/fanyihui/Desktop/web-demo/server/static/articles/JS类型判断.md', '0', '0', '', '2019-1-16 下午6:19:54', '8bb9eea07c');
-INSERT INTO `articles` VALUES ('e01622ffe5', 'BFS求图的最短路径', '1111111', '1', 'admin', '/home/fanyihui/Desktop/web-demo/server/static/articles/BFS求图的最短路径.md', '0', '0', '', '2019-1-16 下午6:19:54', '3a868d8e6b');
+INSERT INTO `articles` VALUES ('06b49d8513', 'js实现插入排序', '', '1', 'admin', '/home/fanyihui/Desktop/web-demo/server/static/articles/js实现插入排序.md', '0', '0', '', '2019-1-16 下午6:19:54', '3a868d8e6b');
+INSERT INTO `articles` VALUES ('6ad6bf1cb4', '动手实现一个简单的Promise', '', '1', 'admin', '/home/fanyihui/Desktop/web-demo/server/static/articles/动手实现一个简单的Promise.md', '0', '0', '', '2019-1-16 下午6:19:54', '15f50360a3');
+INSERT INTO `articles` VALUES ('95e00e13fa', 'Vue生命周期函数', '', '1', 'admin', '/home/fanyihui/Desktop/web-demo/server/static/articles/Vue生命周期函数.md', '4', '0', '', '2019-1-16 下午6:19:54', '6df93151c8');
+INSERT INTO `articles` VALUES ('d04eac5996', 'JS类型判断', '', '1', 'admin', '/home/fanyihui/Desktop/web-demo/server/static/articles/JS类型判断.md', '3', '0', '', '2019-1-16 下午6:19:54', '8bb9eea07c');
+INSERT INTO `articles` VALUES ('e01622ffe5', 'BFS求图的最短路径', '', '1', 'admin', '/home/fanyihui/Desktop/web-demo/server/static/articles/BFS求图的最短路径.md', '0', '0', '', '2019-1-16 下午6:19:54', '3a868d8e6b');
 
 -- ----------------------------
 -- Table structure for comments
@@ -85,8 +85,6 @@ CREATE TABLE `comments` (
 -- Records of comments
 -- ----------------------------
 INSERT INTO `comments` VALUES ('0d9252bfa8', '660c4ddcc0', '0652309127', 'admin', '#icon-default', '11', '', '0', '1547653147656');
-INSERT INTO `comments` VALUES ('12c23865cb', 'b613a33277', '95e00e13fa', 'admin', '#icon-default', '你好啊，嘿嘿', '', '10', '1547693241467');
-INSERT INTO `comments` VALUES ('1c4d91deb2', '', '95e00e13fa', 'emm', '#icon-default13', '牛逼', '', '6', '1547693494039');
 INSERT INTO `comments` VALUES ('1cd84387e1', '65cb082b26', '0652309127', 'admin', '#icon-default', 'sdasadasd', '', '0', '1547652904008');
 INSERT INTO `comments` VALUES ('2a29fe882c', '', '0652309127', 'ad', '#icon-default11', '456688587', '1131153523@qq.com', '0', '1547651197323');
 INSERT INTO `comments` VALUES ('3f96186c92', '660c4ddcc0', '0652309127', 'admin', '#icon-default', '2', '', '0', '1547653296129');
@@ -94,13 +92,12 @@ INSERT INTO `comments` VALUES ('65cb082b26', '', '0652309127', 'ad', '#icon-defa
 INSERT INTO `comments` VALUES ('660c4ddcc0', '', '0652309127', 'ad', '#icon-default14', 'sadasdasd', '1131153523@qq.com', '1', '1547651192602');
 INSERT INTO `comments` VALUES ('72a5dcad7e', 'cd7b30269a', '0652309127', 'admin', '#icon-default', 'asdasd', '', '0', '1547653037546');
 INSERT INTO `comments` VALUES ('837c4dc439', '2a29fe882c', '0652309127', 'admin', '#icon-default', 'asdasd', '', '0', '1547652991687');
-INSERT INTO `comments` VALUES ('96782eb994', 'b613a33277', '95e00e13fa', 'admin', '#icon-default', '哈哈哈哈', '', '73', '1547693446949');
-INSERT INTO `comments` VALUES ('9709f39458', '1c4d91deb2', '95e00e13fa', 'admin', '#icon-default', '6666', '', '1', '1547696101371');
+INSERT INTO `comments` VALUES ('a321c6c937', '', '95e00e13fa', '１２３１２３', '#icon-default8', '１２３１２４１２４', '', '0', '1547818151630');
 INSERT INTO `comments` VALUES ('a7dc59462b', 'cd7b30269a', '0652309127', 'admin', '#icon-default', 'sadasdasd', '', '0', '1547652930354');
-INSERT INTO `comments` VALUES ('b613a33277', '', '95e00e13fa', '123456', '#icon-default9', 'hello', '', '9', '1547693211116');
 INSERT INTO `comments` VALUES ('c0fd181a31', '2a29fe882c', '0652309127', 'admin', '#icon-default', 'sadasdasd', '', '0', '1547652851920');
 INSERT INTO `comments` VALUES ('cd7b30269a', '', '0652309127', 'ad', '#icon-default10', 'asdasd', '1131153523@qq.com', '0', '1547651194291');
-INSERT INTO `comments` VALUES ('d3f52423e4', '660c4ddcc0', '0652309127', 'admin', '#icon-default', '3333', '', '2', '1547653380420');
+INSERT INTO `comments` VALUES ('d3f52423e4', '660c4ddcc0', '0652309127', 'admin', '#icon-default', '3333', '', '3', '1547653380420');
+INSERT INTO `comments` VALUES ('e93d84718e', '', '95e00e13fa', '１２３１２３', '#icon-default7', '１２３１２４１２４ｓａｄａｓ', '', '0', '1547818167367');
 
 -- ----------------------------
 -- Table structure for draft
@@ -113,7 +110,7 @@ CREATE TABLE `draft` (
 -- ----------------------------
 -- Records of draft
 -- ----------------------------
-INSERT INTO `draft` VALUES ('# EventUtil——跨浏览器的事件对象6\ndsadasdss\nsddasdsaasdsadasd');
+INSERT INTO `draft` VALUES ('# dasasdasdasd112');
 
 -- ----------------------------
 -- Table structure for tags
@@ -131,6 +128,7 @@ CREATE TABLE `tags` (
 INSERT INTO `tags` VALUES ('15f50360a3', 'ES6');
 INSERT INTO `tags` VALUES ('324ac48b35', 'Python');
 INSERT INTO `tags` VALUES ('3a868d8e6b', '算法');
+INSERT INTO `tags` VALUES ('3e683567f8', '项目');
 INSERT INTO `tags` VALUES ('5cc303136b', 'Webpack');
 INSERT INTO `tags` VALUES ('6df93151c8', 'Vue');
 INSERT INTO `tags` VALUES ('7148d4c1b2', 'HTML');
@@ -138,3 +136,22 @@ INSERT INTO `tags` VALUES ('8bb9eea07c', 'Javascript');
 INSERT INTO `tags` VALUES ('cdaa231199', 'NodeJS');
 INSERT INTO `tags` VALUES ('d8f4816c34', '数据库');
 INSERT INTO `tags` VALUES ('f11d873b99', 'CSS');
+
+-- ----------------------------
+-- Table structure for tools
+-- ----------------------------
+DROP TABLE IF EXISTS `tools`;
+CREATE TABLE `tools` (
+  `tool_id` varchar(10) NOT NULL,
+  `tool_name` varchar(100) DEFAULT '',
+  `tool_url` varchar(255) DEFAULT '',
+  `tool_type` varchar(30) DEFAULT NULL,
+  PRIMARY KEY (`tool_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of tools
+-- ----------------------------
+INSERT INTO `tools` VALUES ('1', '一套绝佳的图标字体库和CSS框架', 'http://fontawesome.dashgame.com/', '前端');
+INSERT INTO `tools` VALUES ('2', 'CSS3兼容代码自动补全', 'http://pleeease.io/play', '前端');
+INSERT INTO `tools` VALUES ('3', '鸠摩搜书', 'https://www.jiumodiary.com/', '工具');
