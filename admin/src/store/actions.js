b9a31e104822624806880ecc20d45513c1a498cc
1131153523 <1131153523@qq.com>
2019-01-20 15:41:40 +0800

    ...

--- admin/src/store/actions.js
+++ admin/src/store/actions.js
@@ -15,17 +15,16 @@ export default {
             console.log('GET_TAGS出现错误')
         }
     },
-    removeTag({commit}, value){
-        api.removeTag({token: window.sessionStorage.getItem('token'), ...value})
-            .then(res => {
-                if (res.code) {
-                    commit(types.REMOVE_TAG, value)
-                }
-            })
-            .catch(e => {
-                console.log(e);
-                console.log('REMOVE_TAG出现错误')
-            })
+    async removeTag({commit}, value){
+        try {
+            let res = await api.removeTag({token: window.sessionStorage.getItem('token'), ...value})
+            if (res.code) {
+                commit(types.REMOVE_TAG, value)
+            }
+        } catch(e) {
+            console.log(e);
+            console.log('REMOVE_TAG出现错误')
+        }
     },
     async addTag({commit}, value) {
         try {
@@ -45,8 +44,8 @@ export default {
             console.log('ADD_TAG出现错误')
         }
     },
-    getDraft({commit}, value){
-        api.getDraft()
+    async getDraft({commit}, value){
+        let res = await api.getDraft()
             .then((res) => {
                 if (res.code) {
                     commit(types.GET_DRAFT, res.data)
@@ -57,77 +56,71 @@ export default {
                 console.log('GET_DRAFT出现错误')
             })
     },
-    saveDraft({commit}, value) {
-        api.saveDraft({draft_content: value, token: window.sessionStorage.getItem('token')})
-            .then(res => {
-                if (!res.code) {
-                    commit(types.SAVE_DRAFT, value)
-                }
-            })
-            .catch(e => {
-                console.log(e)
-                console.log('SAVE_DRAFT出现错误')
-            })
+    async saveDraft({commit}, value) {
+        try {
+            let res = await api.saveDraft({draft_content: value, token: window.sessionStorage.getItem('token')})
+            if (!res.code) {
+                commit(types.SAVE_DRAFT, value)
+            }
+        } catch (e) {
+            console.log(e)
+            console.log('SAVE_DRAFT出现错误')
+        }
     },
-    writeArticle({commit}, info) {
-        api.writeArticle({...info, token: window.sessionStorage.getItem('token')})
-            .then(res => {
-                if (res.code) {
-                    store.dispatch('getArticleList')
-                }
-            })
-            .catch(e => {
-                console.log(e);
-                console.log('WRITE_ARTICLE出现错误')
-            })        
+    async writeArticle({commit}, info) {
+        try {
+            let res = await api.writeArticle({...info, token: window.sessionStorage.getItem('token')})
+            if (res.code) {
+                store.dispatch('getArticleList')
+            }
+        } catch (e) {
+            console.log(e);
+            console.log('WRITE_ARTICLE出现错误')
+        }
     },
-    getArticleList({commit}) {
-        api.getArticleList()
-            .then(res => {
-                if (res.code) {
-                    commit(types.GET_ARTICLE_LIST, res.data)
-                }
-            })
-            .catch(e => {
-                console.log(e)
-                console.log('GET_ARTICLE_LIST出现错误')
-            })
+    async getArticleList({commit}) {
+        try {
+            let res = await api.getArticleList()
+            if (res.code) {
+                commit(types.GET_ARTICLE_LIST, res.data)
+            }
+        } catch (e) {
+            console.log(e)
+            console.log('GET_ARTICLE_LIST出现错误')
+        }
     },
-    updateArticleVerify({commit}, value) {
-        api.updateArticleVerify({article_id: value, token: window.sessionStorage.getItem('token')})
-            .then(res => {
-                if (res.code) {
-                    commit(types.UPDATE_ARTICLE_VERIFY, value)
-                }
-            })
-            .catch(e => {
-                console.log(e)
-                console.log('UPDATE_ARTICLE_VERIFY出现错误')
-            })
+    async updateArticleVerify({commit}, value) {
+        try {
+            let res = await api.updateArticleVerify({article_id: value, token: window.sessionStorage.getItem('token')})
+            if (res.code) {
+                commit(types.UPDATE_ARTICLE_VERIFY, value)
+            }
+        }catch (e) {
+            console.log(e)
+            console.log('UPDATE_ARTICLE_VERIFY出现错误')  
+        }
     },
-    deleteArticle ({commit}, value) {
-        api.deleteArticle({token: window.sessionStorage.getItem('token'), article_id: value})
-            .then(res => {
-                if (res.code) {
-                    commit(types.DELETE_ARTICLE, value)
-                }   
-            })
-            .catch(e => {
-                console.log(e)
-                console.log('UPDATE_ARTICLE_VERIFY出现错误')
-            })
+    async deleteArticle ({commit}, value) {
+        try {
+            let res = await api.deleteArticle({token: window.sessionStorage.getItem('token'), article_id: value})
+            if (res.code) {
+                commit(types.DELETE_ARTICLE, value)
+            }   
+        } catch (e) {
+            console.log(e)
+            console.log('UPDATE_ARTICLE_VERIFY出现错误')
+        }
     },
-    updateArticle ({commit}, value) {
-        api.updateArticle(value)
-            .then(res => {
-                if (res.code) {
-                    commit(types.UPDATE_ARTICLE, value)
-                }
-            })
-            .catch(e => {
-                console.log(e)
-                console.log('UPDATE_ARTICLE出现错误')
-            })
+    async updateArticle ({commit}, value) {
+        try {
+            let res = await api.updateArticle(value)
+            if (res.code) {
+                commit(types.UPDATE_ARTICLE, value)
+            }
+        } catch (e) {
+            console.log(e)
+            console.log('UPDATE_ARTICLE出现错误')
+        }
     },
     loginGithub({commit}) {
         commit(types.LOGIN_GITHUB)
