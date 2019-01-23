2e3586b70f289a5a283a00d699f7ba58823727dc
1131153523 <1131153523@qq.com>
2019-01-23 11:38:30 +0800

    ...

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -131,19 +131,21 @@ class UserController {
             const access_token = arg[1];
             console.log(body);
             console.log(access_token);
-            return access_token
+            ctx.body = access_token
         })
-        .then(async(token) => {
-            const url = 'https://api.github.com/user?access_token=' + token
-            console.log(url)
-            await fetch(url)
-                .then(res => {
-                    return res.json()
-                })
-                .then(res => {
-                    ctx.body = res
-                })
-        })
+        // .then(async(token) => {
+        //     const url = 'https://api.github.com/user?access_token=' + token
+        //     console.log(url)
+        //     await fetch(url)
+        //         .then(res => {
+        //             return res.json()
+        //         })
+        //         .then(res => {
+        //             ctx.body = {
+
+        //             }
+        //         })
+        // })
     }
     static async addManager (ctx) {
         try {
