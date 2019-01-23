bd81d890b4db6c29ec7ce85d52c62aeb9bb4bf39
1131153523 <1131153523@qq.com>
2019-01-23 11:29:54 +0800

    ...

--- admin/config/index.js
+++ admin/config/index.js
@@ -14,7 +14,8 @@ module.exports = {
         '/': {
             target: 'http://127.0.0.1:3000',
             changeOrigin: true,
-        }
+        },
+        
     },
 
     // Various Dev Server settings
